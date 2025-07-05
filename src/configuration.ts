119873f6d01234d45aa0a7133f8b776f7f0c1dc2
Trương Nguyễn Hoàng <87161976+hoangcute1@users.noreply.github.com>
2025-07-05 13:22:32 +0700

    fix .env

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,5 +3,6 @@ export default () => ({
   JWT_SECRET: process.env.JWT_SECRET || 'defaultSecretKey',
   GOOGLE_EMAIL: process.env.GOOGLE_EMAIL || '',
   GOOGLE_PASSWORD: process.env.GOOGLE_PASSWORD || '',
+
   // Thêm các key khác nếu cần
 });
